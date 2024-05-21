c6717e3c48a79b8d18291503c430f3404a2f4523
Idan Arye <IdanArye@gmail.com>
2024-05-21 19:22:24 +0300

    feat: `require'impairative'.operations` also supports text manipulation (with visual mode and with operators)

---
+++ lua/impairative/_operator_func.lua
@@ -0,0 +1,76 @@
+local M = {}
+
+function M.operatorfunc(range_type)
+    local region_start = vim.fn.getpos("'[")
+    local region_end = vim.fn.getpos("']")
+
+    if range_type == 'char' then
+        local orig_lines = vim.api.nvim_buf_get_text(
+            region_start[1],
+            region_start[2] - 1,
+            region_start[3] - 1,
+            region_end[2] - 1,
+            region_end[3],
+            {}
+        )
+        vim.api.nvim_buf_set_text(
+            region_start[1],
+            region_start[2] - 1,
+            region_start[3] - 1,
+            region_end[2] - 1,
+            region_end[3],
+            M.change_lines(orig_lines)
+        )
+    elseif range_type == 'line' then
+        local orig_lines = vim.api.nvim_buf_get_lines(
+            region_start[1],
+            region_start[2] - 1,
+            region_end[2],
+            true
+        )
+        vim.api.nvim_buf_set_lines(
+            region_start[1],
+            region_start[2] - 1,
+            region_end[2],
+            true,
+            M.change_lines(orig_lines)
+        )
+    elseif range_type == 'block' then
+        vim.cmd.messages('clear')
+
+        for line = region_end[2], region_start[2], -1 do
+            local orig_lines = vim.api.nvim_buf_get_text(
+                region_start[1],
+                line - 1,
+                region_start[3] - 1,
+                line - 1,
+                region_end[3],
+                {}
+            )
+            vim.api.nvim_buf_set_text(
+                region_start[1],
+                line - 1,
+                region_start[3] - 1,
+                line - 1,
+                region_end[3],
+                M.change_lines(orig_lines)
+            )
+        end
+    else
+        error("Unsupported range type " .. vim.inspect(range_type))
+    end
+end
+
+---@param _ string
+---@return string
+function M.change_text(_)
+    error("`require'impairative._operator_func'.change_text` was not set")
+end
+
+function M.change_lines(orig_lines)
+    local orig_text = table.concat(orig_lines, '\n')
+    local new_text = M.change_text(orig_text)
+    return vim.split(new_text, '\n', {plain = true})
+end
+
+return M
--- lua/impairative/operations.lua
+++ lua/impairative/operations.lua
@@ -44,4 +44,27 @@ function ImpairativeOperations:command_pair(args)
     }
 end
 
+local function keybind_text_manipulation_function(keys, func)
+    vim.keymap.set({'n', 'x'}, keys, function()
+        require'impairative._operator_func'.change_text = func
+        vim.o.operatorfunc = "v:lua.require'impairative._operator_func'.operatorfunc"
+        vim.api.nvim_feedkeys('g@', 'ni', false)
+    end)
+end
+
+---@class ImpairativeOperationsTextManipulationArgs
+---@field key string
+---@field name string?
+---@field backward fun(orig: string): string
+---@field forward fun(orig: string): string
+local ImpairativeOperationsTextManipulationArgs
+
+---@param args ImpairativeOperationsTextManipulationArgs
+---@return ImpairativeOperations
+function ImpairativeOperations:text_manipulation(args)
+    keybind_text_manipulation_function(self._opts.backward .. args.key, args.backward)
+    keybind_text_manipulation_function(self._opts.forward .. args.key, args.forward)
+    return self
+end
+
 return ImpairativeOperations
