3fca004e307fc2dbfd02cab449d99d885048f182
Idan Arye <IdanArye@gmail.com>
2024-06-04 18:19:25 +0300

    Document `toggling.lua`

--- lua/impairative/toggling.lua
+++ lua/impairative/toggling.lua
@@ -9,13 +9,14 @@ local ImpairativeTogglingOptions
 local ImpairativeToggling = {}
 
 ---@class ImpairativeTogglingManualArgs
----@field key string
----@field name? string
----@field enable string | fun()
----@field disable string | fun()
----@field toggle string | fun()
+---@field key string The key for the mapping (will be prefixed by one of the leaders)
+---@field name? string A name for generating mapping descriptions
+---@field enable string | fun() Command or function for the enable mapping
+---@field disable string | fun() Command or function for the disable mapping
+---@field toggle string | fun() Command or function for the toggle mapping
 local ImpairativeTogglingManualArgs
 
+---Bind toggling mappings by directly specifying the commands for each mapping.
 ---@param args ImpairativeTogglingManualArgs
 function ImpairativeToggling:manual(args)
     for _, operation in ipairs{'enable', 'disable', 'toggle'} do
@@ -37,12 +38,13 @@ function ImpairativeToggling:manual(args)
 end
 
 ---@class ImpairativeTogglingGetterSetterArgs
----@field key string
----@field name? string
----@field get fun(): boolean
----@field set fun(value: boolean)
+---@field key string The key for the mapping (will be prefixed by one of the leaders)
+---@field name? string A name for generating mapping descriptions
+---@field get fun(): boolean Checks if the toggleable thing is on or off
+---@field set fun(value: boolean) Sets the value of the toggleable thing
 local ImpairativeTogglingGetterSetterArgs
 
+---Bind toggling mappings by directly specifying a getter and a setter.
 ---@param args ImpairativeTogglingGetterSetterArgs
 ---@return ImpairativeToggling
 function ImpairativeToggling:getter_setter(args)
@@ -61,19 +63,22 @@ function ImpairativeToggling:getter_setter(args)
     }
 end
 
----@class (exact) ImpairativeTogglingFieldArgs
----@field key string
----@field name? string
----@field table table
----@field field string
----@field values? {[true]: any, [false]: any}
+---@class ImpairativeTogglingFieldArgs
+---@field key string The key for the mapping (will be prefixed by one of the leaders)
+---@field name? string A name for generating mapping descriptions
+---@field table table The Lua table that contains the field to toggle
+---@field field any The name of the field in the Lua table
+---@field values? {boolean: any} Use when the field is not not using boolean (e.g. `{[true] = 'on', [false] = 'off'}`)
 local ImpairativeTogglingFieldArgs
 
+---Bind toggling mappings for a field in a Lua table.
+---
+---Can also be used for things that use metatables like |lua-vim-variables|,
+---but if that table is |vim.o| prefer using |ImpairativeToggling:option|
+---instead.
 ---@param args ImpairativeTogglingFieldArgs
 ---@return ImpairativeToggling
 function ImpairativeToggling:field(args)
-    local help
-
     local name = args.name
     if name and args.values then
         name = ('%s (on means "%s", off means "%s")'):format(name, args.values[true], args.values[false])
@@ -98,12 +103,13 @@ function ImpairativeToggling:field(args)
     }
 end
 
----@class (exact) ImpairativeTogglingOptionArgs
----@field key string
----@field option string
----@field values? {[true]: any, [false]: any}
+---@class ImpairativeTogglingOptionArgs
+---@field key string The key for the mapping (will be prefixed by one of the leaders)
+---@field option string The name of the Neovim option
+---@field values? {boolean: any} Use when the field is not not using boolean (e.g. `{[true] = 'on', [false] = 'off'}`)
 local ImpairativeTogglingOptionArgs
 
+---Bind toggling mappings for a Neovim option using |vim.o|.
 ---@param args ImpairativeTogglingOptionArgs
 ---@return ImpairativeToggling
 function ImpairativeToggling:option(args)
